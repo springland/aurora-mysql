f7142368e2eebc1b0f15590f6e823b0e8fd14799
fl <fenglinmail@yahoo.com>
2022-04-24 22:47:47 -0400

    Check in for SDK 1.x
    There are a few issues
    1) The SSL verification is turned off , need to double check how is x509 certification used.
    2) I used EC2ContainerCredentialsProviderWrapper which finally uses InstanceProfileCredentialsProvider instead of DefaultAWSCredentialsProviderChain. In theory DefaultAWSCredentialsProviderChain should work

--- src/main/java/com/springland365/auroramysql/AuroraMysqlApplication.java
+++ src/main/java/com/springland365/auroramysql/AuroraMysqlApplication.java
@@ -1,5 +1,6 @@
 package com.springland365.auroramysql;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,18 +12,35 @@ import java.util.Properties;
 @SpringBootApplication
 public class AuroraMysqlApplication implements ApplicationRunner {
 
+
+	@Value("${region}")
+	String region ;
+
+	@Value("${host}")
+	String host ;
+
+	@Value("${port}")
+	int port ;
+
+	@Value("${dbuser}")
+	String dbuser ;
 	public static void main(String[] args) {
 		SpringApplication.run(AuroraMysqlApplication.class, args);
 	}
 
-	private static final String CONNECTION_STRING = "jdbc:mysql:aws://db-identifier.cluster-XYZ.us-east-2.rds.amazonaws.com:3306/employees";
+	private static final String CONNECTION_STRING = "jdbc:mysql://db-identifier.cluster-XYZ.us-east-2.rds.amazonaws.com:3306/employees";
 	private static final String USER = "username";
+
+
+
 	@Override
 	public void run(ApplicationArguments arg0) throws Exception {
 
 		try
 		{
-			runAuroraTest();
+			//runAuroraSDKV2Test();
+			IAMDatabaseAuthentication  auth = new IAMDatabaseAuthentication();
+			auth.runAuroraTest(region , host , port , dbuser);
 		}
 		catch(Exception ex)
 		{
@@ -32,20 +50,26 @@ public class AuroraMysqlApplication implements ApplicationRunner {
 
 	}
 
-	protected void runAuroraTest() throws SQLException
+
+
+	protected void runAuroraSDKV2Test() throws SQLException
 	{
+
+		String jdbcurl = "jdbc:mysql:aws://" + host + ":" + port ;
+
 		System.out.println(" Begin Aurora test");
 
 		final Properties properties = new Properties();
 		// Enable AWS IAM database authentication
 		properties.setProperty("useAwsIam", "true");
-		properties.setProperty("user", USER);
+		properties.setProperty("user", dbuser);
 
-		try (final Connection conn = DriverManager.getConnection(CONNECTION_STRING, properties);
+		try (final Connection conn = DriverManager.getConnection(jdbcurl, properties);
 			 final Statement myQuery = conn.createStatement()) {
-			try (final ResultSet rs = myQuery.executeQuery("SELECT * FROM employees")) {
+			try (final ResultSet rs = myQuery.executeQuery("SELECT * FROM test.employees")) {
 				while (rs.next()) {
-					System.out.println(rs.getString(1));
+					System.out.print(rs.getInt(1));
+					System.out.println("   :    " + rs.getString(2));
 				}
 			}
 		}
---
+++ src/main/java/com/springland365/auroramysql/IAMDatabaseAuthentication.java
@@ -0,0 +1,122 @@
+package com.springland365.auroramysql;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper;
+import com.amazonaws.services.rds.auth.GetIamAuthTokenRequest;
+import com.amazonaws.services.rds.auth.RdsIamAuthTokenGenerator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.KeyStore;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.sql.*;
+import java.util.Properties;
+
+/**
+ * Based on sdk v1.x
+ */
+public class IAMDatabaseAuthentication {
+
+    private static final String SSL_CERTIFICATE = "global-bundle.pem";
+
+    private static final String KEY_STORE_TYPE = "JKS";
+    private static final String KEY_STORE_PROVIDER = "SUN";
+    private static final String KEY_STORE_FILE_PREFIX = "sys-connect-via-ssl-test-cacerts";
+    private static final String KEY_STORE_FILE_SUFFIX = ".jks";
+    private static final String DEFAULT_KEY_STORE_PASSWORD = "changeit";
+
+    public  void runAuroraTest(String region , String host , int port , String user) throws Exception
+    {
+
+
+        Connection  conn = getDBConnectionUsingIam(region , host , port , user);
+
+        //verify the connection is successful
+        final Statement myQuery = conn.createStatement();
+        try (final ResultSet rs = myQuery.executeQuery("SELECT * FROM test.employees")) {
+            while (rs.next()) {
+                System.out.print(rs.getInt(1));
+                System.out.println("   :    " + rs.getString(2));
+            }
+        }
+
+        myQuery.close();
+        conn.close();
+
+        clearSslProperties();
+    }
+
+    private  Connection getDBConnectionUsingIam(String region , String host , int port , String user) throws Exception {
+        String url ="jdbc:mysql://" + host + ":" + port;
+
+        System.out.println(" URL is " + url);
+        setSslProperties();
+        return DriverManager.getConnection(url, setMySqlConnectionProperties(region , host , port ,user));
+    }
+
+    private  void setSslProperties() throws Exception {
+        System.setProperty("javax.net.ssl.trustStore", createKeyStoreFile());
+        System.setProperty("javax.net.ssl.trustStoreType", KEY_STORE_TYPE);
+        System.setProperty("javax.net.ssl.trustStorePassword", DEFAULT_KEY_STORE_PASSWORD);
+    }
+
+    private  String createKeyStoreFile() throws Exception {
+        return createKeyStoreFile(createCertificate()).getPath();
+    }
+    private  File createKeyStoreFile(X509Certificate rootX509Certificate) throws Exception {
+        File keyStoreFile = File.createTempFile(KEY_STORE_FILE_PREFIX, KEY_STORE_FILE_SUFFIX);
+        try (FileOutputStream fos = new FileOutputStream(keyStoreFile.getPath())) {
+            KeyStore ks = KeyStore.getInstance(KEY_STORE_TYPE, KEY_STORE_PROVIDER);
+            ks.load(null);
+            ks.setCertificateEntry("rootCaCertificate", rootX509Certificate);
+            ks.store(fos, DEFAULT_KEY_STORE_PASSWORD.toCharArray());
+        }
+        return keyStoreFile;
+    }
+    private  Properties setMySqlConnectionProperties(String region , String hostname , int port ,String user) {
+        Properties mysqlConnectionProperties = new Properties();
+        mysqlConnectionProperties.setProperty("verifyServerCertificate","false");
+        mysqlConnectionProperties.setProperty("useSSL", "true");
+        mysqlConnectionProperties.setProperty("user",user);
+
+
+        String token = generateAuthToken(region , hostname , port , user);
+
+        System.out.println("User: " + user);
+
+        System.out.println(" Token: " + token);
+        mysqlConnectionProperties.setProperty("password",token);
+        return mysqlConnectionProperties;
+    }
+
+    private  String generateAuthToken(String  region , String hostname , int port ,  String user) {
+
+        RdsIamAuthTokenGenerator generator = RdsIamAuthTokenGenerator.builder()
+                .credentials(new EC2ContainerCredentialsProviderWrapper()).region(region).build();
+        return generator.getAuthToken(GetIamAuthTokenRequest.builder()
+                .hostname(hostname).port(port).userName(user).build());
+    }
+
+
+    private  void clearSslProperties() throws Exception {
+        System.clearProperty("javax.net.ssl.trustStore");
+        System.clearProperty("javax.net.ssl.trustStoreType");
+        System.clearProperty("javax.net.ssl.trustStorePassword");
+    }
+
+
+    private X509Certificate createCertificate() throws Exception {
+        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        URL url = new File(SSL_CERTIFICATE).toURI().toURL();
+        if (url == null) {
+            throw new Exception();
+        }
+        try (InputStream certInputStream = url.openStream()) {
+            return (X509Certificate) certFactory.generateCertificate(certInputStream);
+        }
+    }
+
+}
